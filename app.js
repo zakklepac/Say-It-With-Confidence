f0e56f27b07392da452b875d9ff037780a64614c
zakklepac <41202649+zakklepac@users.noreply.github.com>
2018-08-21 14:56:20 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,49 +1,42 @@
-console.log('booyah')
-//fetch for bsFetch
+document.addEventListener("DOMContentLoaded", () =>{ 
+
+const fortune = document.getElementsByClassName('fortune')[0]
 const proxy = "https://cors-anywhere.herokuapp.com/"
+
 fetch(`${proxy}https://corporatebs-generator.sameerkumar.website/`)
     .then(response => response.json())
     .then(data => {
-        console.log(data)
-        bsFetch = data;
-        return bsFetch
+        console.log(data.phrase)
+        return data.phrase // generated phrase
     })
 
-//split first word from fetch
-function getPexelSearch(bsFetch) {
-    let spacePosition = str.indexOf(' ');
-    if (spacePosition === -1)
-        return str;
-    else
-        return str.substr(0, spacePosition);
-};
-//function creating image and appending it to it's bin
-//fetch(${proxy}  P E X E L)
+    .then(speakPhrase => {
+        fortune.appendChild.innerText(data.phrase) // add phrase to assigned div
+        let TextToSpeechV1 = require('watson-developer-cloud/text-to-speech/v1');
+        let fs = require('fs');    
+        const textToSpeech = new TextToSpeechV1({
+        username: 'zakklepac',
+        password: '!To4ster',
+        url: `${proxy}https://stream.watsonplatform.net/text-to-speech/api/`
+        });
 
-//speech to text fetch
-let TextToSpeechV1 = require('watson-developer-cloud/text-to-speech/v1');
-let fs = require('fs');    
-const textToSpeech = new TextToSpeechV1({
-  username: 'zakklepac',
-  password: '!To4ster',
-  url: `${proxy}https://stream.watsonplatform.net/text-to-speech/api/`
-});
+        let params = {
+        text: `${data.phrase}`,
+        voice: 'en-US_LisaVoice',
+        accept: 'audio/wav'
+        };
 
-let params = {
-  text: `${bsFetch}`,
-  voice: 'en-US_LisaVoice', // Optional voice
-  accept: 'audio/ogg'
-};
-
-// Synthesize speech, correct the wav header, then save to disk
-// (wav header requires a file length, but this is unknown until after the header is already generated and sent)
-textToSpeech
-  .synthesize(params, function(err, audio) {
-    if (err) {
-      console.log(err);
-      return;
-    }
-    textToSpeech.repairWavHeader(audio);
-    fs.writeFileSync('audio.ogg', audio);
-    console.log('audio.ogg written with a corrected ogg header');
-});
+        // Synthesize speech, correct the wav header, then save to disk
+        // (ogg header requires a file length, but this is unknown until after the header is already generated and sent)
+        textToSpeech
+        .synthesize(params, function(err, audio) {
+            if (err) {
+            console.log(err);
+            return;
+            }
+            textToSpeech.repairWavHeader(audio);
+            fs.writeFileSync('audio.wav', audio);
+            console.log('audio.wav written with a corrected ogg header');
+        });
+    })
+})
